72c429134fe72c009bf7d8951d3a6c2a3a9cb586
zhouyg <yunge@taovip.com>
2016-05-12 16:40:59 +0800

    fix and 异步。

--- src/jquery-ui-timepicker-addon.js
+++ src/jquery-ui-timepicker-addon.js
@@ -1,13 +1,8 @@
 /*
  * jQuery Timepicker Addon
  */
-
 (function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		define(['jquery', 'jquery-ui'], factory);
-	} else {
-		factory(jQuery);
-	}
+	factory(jQuery);
 }(function ($) {
 
 	/*
